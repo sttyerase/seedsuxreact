ba25f3277b514684d1bd0ac7ab9e5937d5762f1a
Daniel Moore <sttyerase@gmail.com>
2020-07-08 22:42:45 -0400

    Working display with functions.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function CropUI() {
   return (
     <div className="dataentry">
       <form id="dataform">
-        <label className="datalabel" forName="cropid">Crop Id:</label><br/>
-        <input id="cropid" type="text" className="datainput"/><br/>
-          <label className="datalabel" forName="cropname">Crop Name:</label><br/>
-          <input id="cropname" type="text" className="datainput"/><br/>
-          <label className="datalabel" forName="cropdescription">Crop Description:</label><br/>
-          <input id="cropdescription" type="text" className="datainput"/><br/>
-          <label className="datalabel" forName="cropicccode">Crop ICC Code:</label><br/>
-          <input id="cropicccode" type="text" className="datainput"/><br/>
-          <input className="datasubmit" type="submit" value="PUNCH IT" onClick="putdata()"/>
+        <label className="datalabel" htmlFor="cropid">Crop Id:</label><br/>
+        <input id="cropid" type="text" className="datanumber"/><br/>
+        <label className="datalabel" htmlFor="cropname">Crop Name:</label><br/>
+        <input id="cropname" type="text" className="datatext" defaultValue="Pig Latin"/><br/>
+        <label className="datalabel" htmlFor="cropdescription">Crop Description:</label><br/>
+        <input id="cropdescription" type="text" className="datatext"/><br/>
+        <label className="datalabel" htmlFor="cropicccode">Crop ICC Code:</label><br/>
+        <input id="cropicccode" type="number" className="datanumber" onBlur={validateicccode}/><br/>
+        <div className="buttondiv">
+          <input className="datasubmit" type="button" value="FIND CROP"   formMethod="POST" onClick={findrecord}/>
+          <input className="datasubmit" type="button" value="UPDATE CROP" formMethod="PUT"  onClick={putdata}/>
+          <input className="datasubmit" type="button" value="ADD CROP"    formMethod="POST" onClick={adddata}/>
+        </div>
       </form>
     </div>
   );
 } // FUNCTION APP()
 
-function putdata() {
-  window.open("https://192.168.1.247:8080/seedinspection/crops/all","_blanc");
+function findrecord() {
+  window.open("http://localhost:8080/seedinspection/crops/all");
 } // PUTDATA()
 
-export default App;
+function adddata() {
+  window.open("http://localhost:8080/seedinspection/crops/all");
+} // PUTDATA()
+
+function putdata() {
+  window.open("http://localhost:8080/seedinspection/crops/all");
+} // PUTDATA()
+
+function validateicccode() {
+  var cropcode = document.getElementById("cropicccode").value;
+  console.log(cropcode);
+  if ("" === cropcode || cropcode < 0) {
+    alert("Please enter 0 or a number for the ICC Code.");
+    return false;
+  } // IF
+} // VALIDATEICCCODE()
+
+export default CropUI;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CropUI from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CropUI />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+    <React.StrictMode>
+        <textarea id="resptext" className="resptext" disabled value="HELLO HELLO HELLO"/>
+    </React.StrictMode>,
+    document.getElementById('respdiv')
+);
+
